260600c3e834f3afc872db82097ed570c8009aec
vasandris01 <undris01@gmail.com>
2023-08-30 17:39:51 +0200

    ready for test

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("mijau");
     }
+
+    public static boolean ispáros(int a){
+        return a%2 == 0;
+    }
 }
